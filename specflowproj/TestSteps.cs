cecabfb6489c656015f0c07c91ab287d5dea12c3
ozsoft <ozgurerisir@gmail.com>
2020-08-31 14:06:22 +0100

    remove comments

--- specflowproj/TestSteps.cs
+++ specflowproj/TestSteps.cs
@@ -16,14 +16,7 @@ namespace specflowproj
         [Given("I have entered num into the calculator")]
         public void GivenIHaveEnteredSomethingIntoTheCalculator(Table table)
         {
-            // TODO: implement arrange (recondition) logic
-            // For storing and retrieving scenario-specific data, 
-            // the instance fields of the class or the
-            //     ScenarioContext.Current
-            // collection can be used.
-            // To use the multiline text or the table argument of the scenario,
-            // additional string/Table parameters can be defined on the step definition
-            // method. 
+
 
             foreach (TableRow row in table.Rows)
             {
