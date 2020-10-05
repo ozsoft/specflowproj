12f6c91e3bb1b0337607d4a23ee874f23f6a6909
ozsoft <ozgurerisir@gmail.com>
2020-10-05 09:09:24 +0100

    final

--- specflowproj/StepDefinitions/CreateTimesheetSteps.cs
+++ specflowproj/StepDefinitions/CreateTimesheetSteps.cs
@@ -5,6 +5,10 @@ using specflowproj.Tests;
 using OpenQA.Selenium.Chrome;
 using System;
 
+//to use tables
+using TechTalk.SpecFlow.Assist;
+
+
 //git status : gives you the status of the source code compared to the github rep
 //git add . : this adds the modified files to now be able to be committed
 //git commit -m "comment on checkin" : commenting on the commit
@@ -48,20 +52,20 @@ namespace specflowproj
 
 
         [When("I create new timesheet page with double row using data (.*) (.*) (.*) (.*) (.*)")]
-        public void CreateNewTimesheetPageWithData(string employeeId1, string hourlyRate1, string day1, string hours1, string minutes1)
+        public void CreateNewTimesheetPageWithData(string employeeId, string hourlyRate, string day, string hours, string minutes)
         {
             TimesheetTest ct = new TimesheetTest(driver);
 
-            ct.CreateTestWithMultipleRow(employeeId1, hourlyRate1, day1, hours1, minutes1);
+            ct.CreateTestWithMultipleRow(employeeId, hourlyRate, day, hours, minutes);
         }
 
 
         [Then("The result should be added to the list successfully with (.*) (.*) (.*) (.*) (.*)")]
-        public void TheResultShouldBeAddedToTheListSuccessfully(string employeeId1, string hourlyRate1, string day1, string hours1, string minutes1)
+        public void TheResultShouldBeAddedToTheListSuccessfully(string employeeId, string hourlyRate, string day, string hours, string minutes)
         {
             TimesheetTest ct = new TimesheetTest(driver);
 
-            ct.AssertDetailsAreCorrect(employeeId1, hourlyRate1, day1, hours1, minutes1);
+            ct.AssertDetailsAreCorrect(employeeId, hourlyRate, day, hours, minutes);
 
             driver.Quit();
 
